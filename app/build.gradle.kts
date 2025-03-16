85af54182b8402d3ce05d6f2c3f88d72c4e19a92
uzurpator <nikitazerkalev@gmail.com>
2025-03-16 14:14:29 +0300

    add gradle files

---
+++ app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    id ("com.github.ben-manes.versions") version ("0.52.0")
+    checkstyle
+    application
+    jacoco
+}
+
+//checkstyle {
+//    toolVersion = "10.12.4"
+//}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+}
