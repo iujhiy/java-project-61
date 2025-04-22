1e2e294546d4f577dfac920cf21a8cec6e66e8f4
iujhiy <131986062+iujhiy@users.noreply.github.com>
2025-04-22 08:40:42 +0300

    Update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,11 +1,5 @@
 package hexlet.code;
 
-//import hexlet.code.games.Calculate;
-//import hexlet.code.games.Even;
-//import hexlet.code.games.GCD;
-//import hexlet.code.games.Prime;
-//import hexlet.code.games.Progression;
-
 import java.util.Scanner;
 
 public class App {
